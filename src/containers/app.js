626c261fb75e81714d755b871b59ab8076478df9
Eli Friedman <elifriedman317@gmail.com>
2019-02-16 17:25:37 -0800

    Create app.js

---
+++ src/containers/app.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider, observer } from 'mobx-react';
+import stores from '../lib/root.store';
+
+import Landing from './landing';
+import Navbar from '../components/navbar';
+import '../css/index.css'
+
+
+class App extends Component {
+  render() {
+    return (
+      <Provider {...stores}>
+        <BrowserRouter>
+          <section className="app">
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={Landing} />
+            </Switch>
+          </section>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
+}
+
+export default observer(App);
