d59edbe6e0586354fa7344bd9578da6b1693aca9
Eli Friedman <elifriedman317@gmail.com>
2019-02-17 01:52:47 -0800

    CSS for home page

--- src/containers/app.js
+++ src/containers/app.js
@@ -2,17 +2,21 @@ import React from 'react';
 import { observer } from 'mobx-react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import CodeContainer from './code-container';
+import Labs from './labs';
+import Home from './home';
 
 import Topbar from './topbar'
 import '../styles/app.css'
 
 class App extends React.Component {
   render() {
-    return (      
+    return (
       <BrowserRouter>
         <section className="app">
-          <Topbar/> 
+          <Topbar/>
           <Switch>
+            <Route path="/labs" component = {Labs}/>
+            <Route exact path = "/" component = {Home}/>
             <Route path="/code" component={CodeContainer} />
           </Switch>
         </section>
--- src/containers/labs.js
+++ src/containers/labs.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import "../styles/labs.css";
 
-class Labs extends React.Component {
+export default class Labs extends React.Component {
+
   render () {
         const placeholder = ["proj1", "proj2"];
 
         return (
           <div>
-            {placeholder.map(x => <div>{x}</div>)}
+            {placeholder.map(x => <div id = "labbs">{x}</div>)}
           </div>
         );
   }
