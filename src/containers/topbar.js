2e0d5aff89c7d0799c1817850cfa8386e8e4ceae
Eli Friedman <elifriedman317@gmail.com>
2019-02-16 23:18:18 -0800

    Extend topbar

--- src/containers/topbar.js
+++ src/containers/topbar.js
@@ -9,8 +9,11 @@ class Topbar extends React.Component {
             <nav className="topbar">
                 <ul className="topbar__list">
                     <li><Link to="/">ByteLab</Link></li>
-                    <li><Link to="/code">CODE</Link></li>
-                    <li><Link to="/play">PLAY</Link></li>
+                    <li><Link to="/labs">Labs</Link></li>
+                    <li><Link to="/code">Code</Link></li>
+                    <li><Link to="/sprites">Sprites</Link></li>
+                    <li><Link to="/maps">Maps</Link></li>
+                    <li><Link to="/play">Play</Link></li>
                 </ul>
             </nav>
         );
